3d3fcffaf355838815d8c2d4134318e5f88c8de8
Estudiantes <Estudiantes@Labing70215.udistrital.edu.co>
2017-09-20 17:23:43 -0500

    dghgh

---
+++ Taller/src/taller/Taller.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Taller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int var;
+        int tor ;
+                
+        // TODO code application logic here
+    }
+    
+}
